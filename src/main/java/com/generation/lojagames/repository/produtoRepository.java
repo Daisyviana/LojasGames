82096a9f538f77355235255aa9955e8ca4e6d0c9
Daisy Kelly Viana <daisyviana96@gmail.com>
2023-04-24 15:25:54 -0300

    LojasGames

---
+++ src/main/java/com/generation/lojagames/repository/produtoRepository.java
@@ -0,0 +1,19 @@
+package com.generation.lojagames.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.lojagames.model.Produto;
+
+public interface produtoRepository extends JpaRepository<Produto, Long>{
+	
+	List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome")String nome);
+	
+	List<Produto> findAllByPrecoGreaterThanEqualOrderByPreco(BigDecimal preco);
+	
+	List<Produto> findAllByPrecoLessThanEqualOrderByPrecoDesc(BigDecimal preco);
+
+}
